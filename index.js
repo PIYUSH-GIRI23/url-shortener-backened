15dd890d33acb8613863d7f9065f942e3c5da1ac
PIYUSH GIRI <piyush.giri232003@gmail.com>
2023-09-30 09:27:38 +0530

    changes:

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-require('dotenv').config();
+require('dotenv').config({ path: '.env.local' });
 const app = express();
 const port = process.env.portno;
 const cors = require('cors');
